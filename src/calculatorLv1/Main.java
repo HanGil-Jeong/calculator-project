650a7a8b2687a46561a74e3bb1d4fefdcb50cb7f
HanGil <jhg9240@gmail.com>
2025-02-27 16:26:09 +0900

    ãLv.1계산기 만들기

--- src/calculatorLv1/Main.java
+++ src/calculatorLv1/Main.java
@@ -7,33 +7,60 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner= new Scanner(System.in);
-        System.out.print("숫자를 입력하세요: ");
-        int number1 = scanner.nextInt();
-        System.out.println(number1);
+        int number1;
+        int number2;
+        String operation;
 
-        System.out.print("+, -, X, % 중 선택하세요: ");
-        String inputStr = scanner.next();
+        while(true) {
+            System.out.print("숫자를 입력해주세요: ");
+            number1=scanner.nextInt();
+            System.out.println(number1);
 
-        System.out.println(number1 + inputStr);
+            System.out.println("-----------");
+            System.out.println("+, -, x, %");
+            System.out.println("-----------");
+            System.out.print("사칙연산 선택: ");
+            operation=scanner.next();
 
-        System.out.print("숫자를 입력하세요: ");
-        int number2 = scanner.nextInt();
+            while (true){
+                System.out.print("숫자를 입력해주세요: ");
+                number2 =scanner.nextInt();
+                System.out.println(number1 + operation + number2);
 
-        if (inputStr.equals("+")) {
-            int sum = number1 + number2;
-            System.out.println(number1 + inputStr + number2 + "=" + sum);
-        } else if (inputStr.equals("-")) {
-            int sub = number1 - number2;
-            System.out.println(number1 + inputStr + number2 + "=" + sub);
-        } else if (inputStr.equals("X")) {
-            int mul = number1 * number2;
-            System.out.println(number1 + inputStr + number2 + "=" + mul);
-        } else if (inputStr.equals("%")) {
-            int div = number1 / number2;
-            System.out.println(number1 + inputStr + number2 + "=" + div);
-        } else {
-            System.out.println("잘못 누르셨습니다.");
+                if(operation.equals("+")) {
+                    int result = number1 + number2;
+                    System.out.println(number1 + operation + number2 + "=" +result);
+                    break;
+                } else if(operation.equals("-")) {
+                    int result = number1 - number2;
+                    System.out.println(number1 + operation + number2 + "=" +result);
+                    break;
+                } else if(operation.equals("x") || operation.equals("*")) {
+                    int result = number1 * number2;
+                    System.out.println(number1 + operation + number2 + "=" +result);
+                    break;
+                } else if(operation.equals("%") || operation.equals("/")) {
+                    if (number2 != 0) {
+                        int result = number1 / number2;
+                        System.out.println(number1 + operation + number2 + "=" +result);
+                        break;
+                    } else {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                    }
+                } else {
+                    System.out.println("잘못 누르셨습니다.");
+                }
+            }
+
+            System.out.print("더 계산하시겠습니까? (exit입력 시 종료)");
+            String answer=scanner.next();
+            if (answer.equals("exit")) {
+                break;
+            }
+
         }
+
+
 
 
     }
